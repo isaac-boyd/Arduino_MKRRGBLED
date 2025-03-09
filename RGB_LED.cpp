343e108bdba2b4a1115150069f22b5d323060982
Isaac Boyd <isaacjonathanboyd@gmail.com>
2025-03-09 13:41:50 -0400

    Create RGB_LED.cpp

---
+++ RGB_LED.cpp
@@ -0,0 +1,16 @@
+#include <WiFiNINA.h>
+#include <utility/wifi_drv.h>
+RGB_LED::RGB_LED(){}
+void RGB_LED::red(){}
+void RGB_LED::orange(){}
+void RGB_LED::yellow(){}
+void RGB_LED::green(){}
+void RGB_LED::blue(){}
+void RGB_LED::purple(){}
+void RGB_LED::pink(){}
+void RGB_LED::gold(){}
+void RGB_LED::customColor(){
+  WiFiDrv::analogWrite(25, R);
+  WiFiDrv::analogWrite(26, G);
+  WiFiDrv::analogWrite(27, B);
+}
