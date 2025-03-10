01eef82c15ec383d5e672e3c7a0fcf5853c2022a
Isaac Boyd <isaacjonathanboyd@gmail.com>
2025-03-09 20:44:36 -0400

    Update RGB_LED.cpp final

--- RGB_LED.cpp
+++ RGB_LED.cpp
@@ -1,18 +1,64 @@
 #include <WiFiNINA.h>
 #include <utility/wifi_drv.h>
-#inlcude "RGB_LED.h"
-RGB_LED::RGB_LED(){}
-void RGB_LED::red(){}
-void RGB_LED::orange(){}
-void RGB_LED::yellow(){}
-void RGB_LED::green(){}
-void RGB_LED::blue(){}
-void RGB_LED::purple(){}
-void RGB_LED::pink(){}
-void RGB_LED::gold(){}
-void RGB_LED::customColor(){
-  WiFiDrv::analogWrite(25, R);
-  WiFiDrv::analogWrite(26, G);
-  WiFiDrv::analogWrite(27, B);
+#include "RGB_LED.h"
+
+RGB_LED::RGB_LED() {}
+void RGB_LED::begin(){
+  WiFiDrv::pinMode(25, OUTPUT); //define green pin
+  WiFiDrv::pinMode(26, OUTPUT); //define red pin
+  WiFiDrv::pinMode(27, OUTPUT); //
+}
+void RGB_LED::red() {
+    WiFiDrv::analogWrite(25, 255);
+    WiFiDrv::analogWrite(26, 0);
+    WiFiDrv::analogWrite(27, 0);
+}
+
+void RGB_LED::orange() {
+    WiFiDrv::analogWrite(25, 255);
+    WiFiDrv::analogWrite(26, 165);
+    WiFiDrv::analogWrite(27, 0);
+}
+
+void RGB_LED::yellow() {
+    WiFiDrv::analogWrite(25, 255);
+    WiFiDrv::analogWrite(26, 255);
+    WiFiDrv::analogWrite(27, 0);
+}
+
+void RGB_LED::green() {
+    WiFiDrv::analogWrite(25, 0);
+    WiFiDrv::analogWrite(26, 255);
+    WiFiDrv::analogWrite(27, 0);
+}
+
+void RGB_LED::blue() {
+    WiFiDrv::analogWrite(25, 0);
+    WiFiDrv::analogWrite(26, 0);
+    WiFiDrv::analogWrite(27, 255);
+}
+
+void RGB_LED::purple() {
+    WiFiDrv::analogWrite(25, 160);
+    WiFiDrv::analogWrite(26, 32);
+    WiFiDrv::analogWrite(27, 240);
+}
+
+void RGB_LED::pink() {
+    WiFiDrv::analogWrite(25, 255);
+    WiFiDrv::analogWrite(26, 192);
+    WiFiDrv::analogWrite(27, 203);
+}
+
+void RGB_LED::gold() {
+    WiFiDrv::analogWrite(25, 255);
+    WiFiDrv::analogWrite(26, 215);
+    WiFiDrv::analogWrite(27, 0);
+}
+
+void RGB_LED::customColor(int R, int G, int B) {
+    WiFiDrv::analogWrite(25, R);
+    WiFiDrv::analogWrite(26, G);
+    WiFiDrv::analogWrite(27, B);
 }
  
